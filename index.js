1d01d7dfadd85f92de8dcc833c0f7a7cac7636e0
lisswart <arnoudlisa@gmail.com>
2021-03-16 01:18:18 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your code here!
+/*const element = document.createElement('div');
+
+document.body.appendChild(element);
+
+const ul = document.createElement('ul');
+
+for (let i = 0; i < 3; i++) {
+    const li = document.createElement('li');
+    li.innerHTML = (i + 1).toString();
+    ul.appendChild(li);
+}
+
+element.appendChild(ul);
+element.style.backgroundColor = '#27647B';
+element.style.fontSize = '24px';
+element.style.marginLeft = '70px';
+element.style.marginRight = '70px';
+
+element.style.lineHeight = 2;*/
+
+const main =document.getElementById('main');
+main.remove();
+
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Lisa is the champion';
+newHeader.style.marginLeft = '80px';
+
